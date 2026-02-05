4d3dd77ebbd2cd70e0e6b1bd0245bdbe4285ec7e
IMOGEN MACMILLAN <2501170@uad.ac.uk>
2026-02-05 14:30:53 +0000

    Refactoring Sheep, and beginning adding map boundaries

--- Coursework/CMP105App/Sheep.cpp
+++ Coursework/CMP105App/Sheep.cpp
@@ -40,129 +40,40 @@ Sheep::~Sheep()
 
 void Sheep::handleInput(float dt)
 {
-	// set sheep direction
-	// decrement and check the input buffer.
-	m_inputBuffer -= dt;
-	if (m_inputBuffer > 0)
-	{
-		// not long enough has passed since the last input change, so don't handle input
-		return;
-	}
-	// grab this to detect changes per frame for later
-	Direction last_dir = m_direction;
+	
+	sf::Vector2f inputDir ({ 0,0 });
 
-	// sheep brake
-	if (m_input->isKeyDown(sf::Keyboard::Scancode::Space))
-	{
-		m_direction = Direction::NONE;
-		return;
-	}
-
-	// Set 8-directional movement based on WASD
 	if (m_input->isKeyDown(sf::Keyboard::Scancode::A))
 	{
-		if (m_input->isKeyDown(sf::Keyboard::Scancode::W))
-		{
-			m_direction = Direction::UP_LEFT;
-			m_currentAnimation = &m_walkUpRight;
-			m_currentAnimation->setFlipped(true);
-		}
-
-
-		else if (m_input->isKeyDown(sf::Keyboard::Scancode::S))
-		{
-			m_direction = Direction::DOWN_LEFT;
-			m_currentAnimation = &m_walkDownRight;
-			m_currentAnimation->setFlipped(true);
-		}
-		else
-		{
-			m_direction = Direction::LEFT;
-			m_currentAnimation = &m_walkRight;
-			m_currentAnimation->setFlipped(true);
-		}
-
+		inputDir = { -1,0 };
+		m_acceleration = inputDir * ACCELERATION;
 	}
-	else if (m_input->isKeyDown(sf::Keyboard::Scancode::D))
+	if (m_input->isKeyDown(sf::Keyboard::Scancode::D))
 	{
-		if (m_input->isKeyDown(sf::Keyboard::Scancode::W))
-		{
-			m_direction = Direction::UP_RIGHT;
-			m_currentAnimation = &m_walkUpRight;
-			m_currentAnimation->setFlipped(false);
-		}
-		else if (m_input->isKeyDown(sf::Keyboard::Scancode::S))
-		{
-			m_direction = Direction::DOWN_RIGHT;
-			m_currentAnimation = &m_walkDownRight;
-			m_currentAnimation->setFlipped(false);
-		}
-		else
-		{
-			m_direction = Direction::RIGHT;
-			m_currentAnimation = &m_walkRight;
-			m_currentAnimation->setFlipped(false);
-		}
-
+		inputDir = { 1,0 };
+		m_acceleration = inputDir * ACCELERATION;
 	}
-	else
+
+	if (m_input->isKeyDown(sf::Keyboard::Scancode::W))
 	{
-		if (m_input->isKeyDown(sf::Keyboard::Scancode::W))
-		{
-			m_direction = Direction::UP;
-			m_currentAnimation = &m_walkUp;
-
-		}
-
-		else if (m_input->isKeyDown(sf::Keyboard::Scancode::S))
-		{
-			m_direction = Direction::DOWN;
-			m_currentAnimation = &m_walkDown;
-		}
+		inputDir = { 0,-1 };
+		m_acceleration = inputDir * ACCELERATION;
 	}
 
-	// set input buffer if needed, this makes diagonal movement easier
-	if (m_direction != last_dir)
-		m_inputBuffer = INPUT_BUFFER_LENGTH;
+	if (m_input->isKeyDown(sf::Keyboard::Scancode::S))
+	{
+		inputDir = { 0,1 };
+		m_acceleration = inputDir * ACCELERATION;
+	}
 }
 
 
 void Sheep::update(float dt)
 {
-	setTextureRect(m_currentAnimation->getCurrentFrame());
-	if (m_direction != Direction::NONE)
-		m_currentAnimation->animate(dt);
+	m_velocity = (m_velocity + (m_acceleration * 1.f))* DRAG_FACTOR;
+	move(m_velocity * dt);
+}
 
-	// move the sheep
-	// for diagonal movement
-	float diagonal_speed = m_speed * APPROX_ONE_OVER_ROOT_TWO * dt;
-	float orthog_speed = m_speed * dt;	// orthogonal movement
-
-	switch (m_direction)
-	{
-	case Direction::UP:
-		move({ 0, -orthog_speed });
-		break;
-	case Direction::UP_RIGHT:
-		move({ diagonal_speed, -diagonal_speed });
-		break;
-	case Direction::RIGHT:
-		move({ orthog_speed,0 });
-		break;
-	case Direction::DOWN_RIGHT:
-		move({ diagonal_speed, diagonal_speed });
-		break;
-	case Direction::DOWN:
-		move({ 0, orthog_speed });
-		break;
-	case Direction::DOWN_LEFT:
-		move({ -diagonal_speed, diagonal_speed });
-		break;
-	case Direction::LEFT:
-		move({ -orthog_speed,0 });
-		break;
-	case Direction::UP_LEFT:
-		move({ -diagonal_speed, -diagonal_speed });
-		break;
-	}
+void Sheep::SetWorldSize()
+{
 }
--- Coursework/CMP105App/Sheep.h
+++ Coursework/CMP105App/Sheep.h
@@ -12,20 +12,20 @@ public:
 
 	void handleInput(float dt) override;
 	void update(float dt) override;
+	void SetWorldSize();
 
 private:
-	enum class Direction { UP, DOWN, LEFT, RIGHT, UP_RIGHT, DOWN_RIGHT, DOWN_LEFT, UP_LEFT, NONE };
-	Direction m_direction = Direction::NONE;
-	float m_speed = 300.0f;
-	float m_inputBuffer = 0.f;
-	const float INPUT_BUFFER_LENGTH = 0.1f;
-	const float APPROX_ONE_OVER_ROOT_TWO = 0.70710678f;	// 1 / sqrt(2)
+	
+	const float ACCELERATION = 30.0f;
+	const float DRAG_FACTOR = 0.95f;
+	sf::Vector2f m_acceleration;
+	sf::Vector2f m_worldSize;
 
 	Animation m_walkDown;
 	Animation m_walkUp;
 	Animation m_walkUpRight;
 	Animation m_walkRight;
-	Animation m_walkDownRight;
+	Animation m_walkDownRight;    
 
 	Animation* m_currentAnimation;
 };
